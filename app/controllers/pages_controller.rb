5e3b7dbd8a8df143f679bfdd55c2356bfd79299e
Antoine Loron <loron.antoine@gmail.com>
2016-04-11 10:47:16 +0200

    Landing first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+  # skip_before_action :authenticate_user!, only: :home
+  # skip_after_action :verify_authorized, only: :home
+  # before_filter :disable_flashes, only: [:home]
+  # before_filter :disable_navbar, only: [:home]
+
+  def home
+  end
+end
