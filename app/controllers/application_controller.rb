640106d2b504b5fc741f44f1d8f6d43aecbfcbb3
Antoine Loron <loron.antoine@gmail.com>
2016-06-08 15:36:35 +0200

    HTTP password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  http_basic_authenticate_with :name => "matchmed", :password => "paris2303"
+
   def default_url_options
     { host: ENV['HOST'] || 'localhost:3000' }
   end
--- app/models/suscriber.rb
+++ app/models/suscriber.rb
@@ -1,5 +1,5 @@
 class Suscriber < ActiveRecord::Base
 
-  #validates :email, uniqueness: true
+  validates :email, presence: true
 
 end
