50195556605573dafc924612a3d26ca13fef3988
Antoine Loron <loron.antoine@gmail.com>
2016-06-08 13:33:08 +0200

    LP with carousel

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -44,4 +44,20 @@ $(document).ready(function(){
     $('#how-locums').removeClass('hidden');
   });
 
+  // Smooth scrolling when clicking on anchor links
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 });
+
